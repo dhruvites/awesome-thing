d070b680cb249e683e977c65d9761462875fc0e3
dhruvites <dhruvites@gmail.com>
2022-12-27 17:43:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Card Flip
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hellomp/pen/ZvrmdN](https://codepen.io/hellomp/pen/ZvrmdN).
+
+A simple card flip effect using Anime.js
---
+++ src/scriptHome.js
@@ -0,0 +1,59 @@
+function init(){  
+var current = null;
+document.querySelector('#email').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#password').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#submit').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '530 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+
+document.querySelector('#submit').addEventListener('click', function(e) {
+  console.log(123);
+  window.location.href="index.html";
+});
+
+}
+
+window.onload = init; 
